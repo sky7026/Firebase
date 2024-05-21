795f1a85efa1eb63c794ac0299c6d9a86781da7d
Eshan Chathuranga <84018999+sky7026@users.noreply.github.com>
2024-05-21 22:25:35 +0000

    Update index.js

--- Email_and_password_signup_and_login/index.js
+++ Email_and_password_signup_and_login/index.js
@@ -3,13 +3,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.1/firebas
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAMakMcHyAt58UZl-3t6GioHian7443kyE",
-    authDomain: "fir-test-72256.firebaseapp.com",
-    projectId: "fir-test-72256",
-    storageBucket: "fir-test-72256.appspot.com",
-    messagingSenderId: "1044814205284",
-    appId: "1:1044814205284:web:7b6305d8fe95b7a2f7ea71",
-    measurementId: "G-QHDK84633W"
+  //Paste your firebase Config here
   }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
